b49f988b9e5e001a8433a39ff28b859d02954932
etgit config --global user.name Your <teferaetsegenet@gmail.com.com>
2024-02-15 02:57:47 -0500

    added code Auth

--- amazon/src/App.js
+++ amazon/src/App.js
@@ -1,7 +1,36 @@
+import { useEffect, useContext, } from 'react';
 import './App.css';
 import Routing from './Router';
+import { Type } from './Utility/action.type';
+import {auth } from "./Utility/firebase"
+import {DataContext} from "./Component/DataProvider/DataProvider.jsx"
+
+
+
 
 function App() {
+const [{user}, dispatch] = useContext (DataContext)
+useEffect (()=>{
+auth.onAuthStateChanged((authUser)=>{
+  if(authUser){
+    // console.log(authUser);
+    dispatch({
+      type:Type.SET_USER,
+      user:authUser
+    })
+  }else{
+    dispatch({
+      type:Type.SET_USER,
+      user:null
+    });
+  }
+});
+
+
+},[]);
+
+
+
   return (
     <div>
       <Routing />
