05ab445111e87420265b1058fec063f7ef3ad8aa
etgit config --global user.name Your <teferaetsegenet@gmail.com.com>
2024-02-15 01:29:56 -0500

    added component Auth error catch and modified css

--- amazon/src/pages/Auth/Auth.jsx
+++ amazon/src/pages/Auth/Auth.jsx
@@ -3,6 +3,7 @@ import classes from "./signup.module.css"
 import { Link } from 'react-router-dom';
 import { auth } from "../../Utility/firebase";
 import {signInWithEmailAndPassword, createUserWithEmailAndPassword} from "firebase/auth"
+import { ClipLoader } from 'react-spinners';
 import { DataContext } from '../../Component/DataProvider/DataProvider';
 import { Type } from '../../Utility/action.type';
 
@@ -11,6 +12,10 @@ function Auth() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [loading,setLoading] = useState({
+    signIn: false,
+    signUp: false
+  })
 
   const [{user}, dispatch] = useContext(DataContext)
 
@@ -21,28 +26,36 @@ e.preventDefault()
 console.log(e.target.name);
 if (e.target.name == "signin") {
   // firebase auth 
+
+  setLoading({...loading, signIn:true})
   signInWithEmailAndPassword(auth, email, password).then((userInfo)=>{
     // console.log(userInfo);
     dispatch({
       type: Type.SET_USER,
       user:userInfo.user 
-    })
+    });
+    setLoading({...loading, signIn:false})
   })
   .catch((err)=>{
     setError(err.message);
+    setLoading({...loading, signIn:false})
 
   })
 
 }else{
+  setLoading({...loading, signUp:true})
   createUserWithEmailAndPassword(auth, email, password).then((userInfo) =>{
     // console.log(userInfo);
+   
     dispatch({
       type: Type.SET_USER,
       user:userInfo.user 
-    })
+    });
+    setLoading({...loading, signUp:false})
   })
   .catch((err) =>{
     setError(err.message);
+    setLoading({...loading, signUp:false})
   })
 }
   }
@@ -62,14 +75,33 @@ if (e.target.name == "signin") {
             <div>
               
               <label htmlFor="email">Email</label>
-              <input value={email} onChange={(e)=>setEmail(e.target.value)} type="email" id='email' />
+              <input value={email} 
+              onChange={(e)=>setEmail(e.target.value)} 
+              type="email" id='email' 
+              />
             </div>
             <div>
               
               <label htmlFor="password">Password</label>
-              <input value={password} onChange={(e)=>setPassword(e.target.value)} type="password" id='password' />
+              <input 
+              value={password} 
+              onChange={(e)=>setPassword(e.target.value)} 
+              type="password" 
+              id='password' 
+              />
             </div>
-            <button type="submit" onClick={authHandler} name="signin" lassName={classes.login__signInButton}>Sign In</button>
+            <button 
+            type="submit" 
+            onClick={authHandler} 
+            name="signin" 
+            className={classes.login__signInButton}
+            >
+              {loading.signIn ? ( 
+              <ClipLoader color="#000" size={15}></ClipLoader>
+              ):(
+                "Sign In"
+                )}
+                </button>
 
           </form>
           {/*agreemet*/}
@@ -81,12 +113,11 @@ if (e.target.name == "signin") {
           <button type= "submit" onClick={authHandler} name="signup"  className={classes.login__registerButton}>Create your Amazon Account
           
           </button>
-         { error && <small>{error}</small>}
+          { error && ( 
+          <small style={{paddingTop: "5px", color: "red"}}>{error}</small>
+          )}
         </div>
-
-      
         </section>
-     
     );
 }
 
